da8ada6fc1c4081f400a08ec2cab7cb96e819eb2
AnnBelousova <71022300+AnnBelousova@users.noreply.github.com>
2023-10-12 12:19:38 +0300

    Update ScheduleTask.java

--- src/main/java/pro/sky/telegrambot/service/ScheduleTask.java
+++ src/main/java/pro/sky/telegrambot/service/ScheduleTask.java
@@ -5,7 +5,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import java.time.LocalDateTime;
 
 public interface ScheduleTask {
-    @Scheduled(cron = "0 0/1 * * * *")
     void findNotificationFromDb();
     void saveNotificationToDb(long chatId, String notification, LocalDateTime date);
 }
